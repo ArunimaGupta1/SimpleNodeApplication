3547dbe4e496279c3fcdc6984225a851cb29e628
Arunima Gupta <arunimagupta71@gmail.com>
2021-12-07 11:38:18 +0530

    Simple Node Application

---
+++ consoleDemo.js
@@ -0,0 +1,5 @@
+console.log("This is a simple log.");
+console.info("This is a simple log.");
+console.warn("This is a simple warn.");
+console.error("Error here");
+//console.error(new Error("Error generated!"));
